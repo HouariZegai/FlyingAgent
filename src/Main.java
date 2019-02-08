e862f5db31935a6c18285042256f24ff8dc98df3
Younes Charfaoui <mxcsyounes@gmail.com>
2019-02-08 22:04:41 +0100

    Final test of the informations classes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
-import com.google.gson.Gson;
-import information.MemoryInformation;
+import information.*;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(new CPUInformation().toString());
+        System.out.println(new DisksInformation().toString());
+        System.out.println(new MemoryInformation().toString());
+        System.out.println(new NetworkInformation().toString());
+        System.out.println(new OSInformation().toString());
     }
 }
