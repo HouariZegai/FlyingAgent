dbc05e1e870aa03a84884d57e624d83de3ca61f3
HouariZEGAI <houarizegai14@gmail.com>
2019-02-22 21:13:32 +0100

    Refactoring jade constant information

---
+++ src/utils/Constants.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class Constants {
+    /* Jade information */
+    public static final String MAIN_CONTAINER_HOST_IP = "192.168.1.2";
+    public static final int MAIN_CONTAINER_PORT = 9999;
+    public static final String PLATFORM_ID = "FlyingAgents";
+}
