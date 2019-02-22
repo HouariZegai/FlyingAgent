849ff4a03008d6824e1109fef3e9196bc33edae2
HouariZEGAI <houarizegai14@gmail.com>
2019-02-22 19:11:08 +0100

    Ignore unused files & cleaning

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,14 +1,17 @@
 import agents.MainAgent;
 import agents.MobileAgent;
+import controllers.MainController;
 import controllers.ScanEachController;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.core.Runtime;
 import jade.wrapper.AgentContainer;
 import jade.wrapper.AgentController;
+import jade.wrapper.ControllerException;
 import jade.wrapper.StaleProxyException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -17,11 +20,10 @@ import java.io.IOException;
 
 public class Launcher extends Application {
 
-    private static final String MAIN_CONTAINER_HOST_IP = "192.168.43.36";
-    private static final int MAIN_CONTAINER_PORT = 1099;
+    private static final String MAIN_CONTAINER_HOST_IP = "192.168.1.2";
+    private static final int MAIN_CONTAINER_PORT = 1091;
     private static final String PLATFORM_ID = "FlyingAgents";
     private static AgentContainer mc;
-
 
     public static void main(String[] args) {
         launch(args);
@@ -30,24 +32,33 @@ public class Launcher extends Application {
     @Override
     public void start(Stage stage) {
         try {
-
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/views/ScanEach.fxml"));
             Parent root = loader.load();
-            stage.setScene(new Scene(root));
 
             ScanEachController homeController = loader.getController();
+            initAgent(homeController);
 
-            // Get a hold on JADE runtime
-            Runtime rt = Runtime.instance();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Flying Agent");
+            stage.show();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
 
-            // Exit the JVM when there are no more containers around
-            rt.setCloseVM(true);
+    private void initAgent(ScanEachController controller) {
+        // Get a hold on JADE runtime
+        Runtime rt = Runtime.instance();
 
-            // Launch a complete platform on the 8888 port
-            // create a default Profile
-            Profile pMain = new ProfileImpl(MAIN_CONTAINER_HOST_IP, MAIN_CONTAINER_PORT, PLATFORM_ID);
-            mc = rt.createMainContainer(pMain);
+        // Exit the JVM when there are no more containers around
+        rt.setCloseVM(true);
 
+        // Launch a complete platform on the 8888 port
+        // create a default Profile
+        Profile pMain = new ProfileImpl(MAIN_CONTAINER_HOST_IP, MAIN_CONTAINER_PORT, PLATFORM_ID);
+        mc = rt.createMainContainer(pMain);
+
+        try {
             AgentController receiverAgent = mc.createNewAgent(MainAgent.NAME, MainAgent.class.getName(), new Object[]{});
             receiverAgent.start();
 
@@ -56,23 +67,11 @@ public class Launcher extends Application {
 
             AgentController mobileAgent = mc.createNewAgent("Service-Agent", MobileAgent.class.getName(), new Object[]{});
             mobileAgent.start();
-            homeController.setMainAgentController(receiverAgent);
-            MainAgent.setHomeController(homeController);
-        } catch (IOException | StaleProxyException ioe) {
-            ioe.printStackTrace();
+            controller.setMainAgentController(receiverAgent);
+            MainAgent.setHomeController(controller);
+        } catch(StaleProxyException spe) {
+            spe.printStackTrace();
         }
-
-
-        stage.setTitle("Flying Agent");
-        stage.show();
-        stage.setOnCloseRequest(e -> {
-            try {
-                //mc.kill();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        });
     }
-
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,9 @@
-import javafx.application.Application;
 import information.AllInformation;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-import java.io.IOException;
+public class Main {
 
-public class Main extends Application {
-
-    public static void mains(String[] args) throws IOException {
+    public static void main(String[] args) {
         AllInformation information = new AllInformation();
         System.out.println(information.toString());
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/resources/views/Main.fxml"));
-
-        stage.setScene(new Scene(root));
-        stage.show();
     }
 }
