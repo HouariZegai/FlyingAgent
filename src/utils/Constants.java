e2551edfb69b78de661c5f204dbcd9dd794f540f
Younes Charfaoui <mxcsyounes@gmail.com>
2019-07-07 21:54:36 +0100

    Making the final report

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -2,7 +2,7 @@ package utils;
 
 public class Constants {
     /* Jade information */
-    public static final String MAIN_CONTAINER_HOST_IP = "192.168.1.33";
+    public static final String MAIN_CONTAINER_HOST_IP = "192.168.43.36";
     public static final int MAIN_CONTAINER_PORT = 1099;
     public static final String PLATFORM_ID = "FlyingAgents";
 }
